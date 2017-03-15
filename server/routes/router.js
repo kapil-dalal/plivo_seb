7061a78827e34c29501bb724c91f8c22c5673dbc
Kapil <Kapil>
2017-03-15 22:42:13 +0530

    transfer call to free agent

--- server/routes/router.js
+++ server/routes/router.js
@@ -184,7 +184,7 @@ function outboundCall(request, response) {
 
    var errorMsg = "Something happen wrong. please try after some time";
    var connectingMessage = "connecting your call.";
-
+   var r = plivo.Response();
    try {
       var query = { $table: constants.SCHEMA_NAMES.AGENTS, $filter: constants.SCHEMA_AGENTS.SIP + ' = "' + from + '"' };
       dbService.query(query, function (err, agentResult) {
@@ -220,7 +220,7 @@ function outboundCall(request, response) {
             console.log('confrence_callback callDetailsResult: ', callDetailsResult);
             var from = data.From;
             var to = data.To;
-            var r = plivo.Response();
+
 
             var agentStatusUpdate = {}
             agentStatusUpdate[constants.SCHEMA_AGENT_STATUS.STATUS_ID] = constants.AGENT_STATUS_TYPES.ENGAGED;
@@ -245,6 +245,7 @@ function outboundCall(request, response) {
          }
       });
    } catch (err) {
+      console.log('err: ', err);
       r.addSpeak(errorMsg);
       sendResponse(response, r);
    }
