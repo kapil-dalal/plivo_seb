4323d0ebe1174ad1dc77cd0af7f2f367e08903f8
Kapil <Kapil>
2017-03-15 23:13:30 +0530

    transfer call to free agent

--- server/routes/router.js
+++ server/routes/router.js
@@ -235,6 +235,8 @@ function outboundCall(request, response) {
             var record_params = {
                'action': 'https://35.165.241.189:3010/record_action/', // Submit the result of the record to this URL
                'method': "GET", // HTTP method to submit the action URL
+               redirect: "false",
+               recordSession: true,
                // 'callbackUrl': "https://intense-brook-8241.herokuapp.com/record_callback/", // If set, this URL is fired in background when the recorded file is ready to be used.
                // 'callbackMethod': "GET" // Method used to notify the callbackUrl.
             }
